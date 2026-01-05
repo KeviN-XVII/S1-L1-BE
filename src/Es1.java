62f31f4bb28be00784e6d49878a1e47edff50232
KeviN-XVII <kevinquartar@gmail.com>
2026-01-05 18:33:23 +0100

    Es1 n3 aggiornato

--- src/Es1.java
+++ src/Es1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Es1 {
     public static void main (String[] args) {
 //       Moltiplica
@@ -12,7 +14,22 @@ public class Es1 {
         int numeriS = 7;
         System.out.println(settimana + numeriS);
 //        Array
-        String[] arrayS = {"Lunedi","Martedi","Giovedi","Venerdi","Sabato"};
-        String giorno = "Mercoledi";
+        String[] array = {"Lunedi", "Martedi", "Giovedi", "Venerdi", "Sabato"};
+        String nuova = "Mercoledi";
+
+        String[] risultatoArray = listaStringhe (array, nuova);
+        System.out.println(Arrays.toString(risultatoArray));
     }
+        public static String[] listaStringhe (String[] array, String nuovaStringa) {
+            String[] risultato = new String[6];
+
+            risultato[0] = array[0];
+            risultato[1] = array[1];
+            risultato[2] = nuovaStringa;
+            risultato[3] = array[2];
+            risultato[4] = array[3];
+            risultato[5] = array[4];
+
+            return risultato;
+        }
 }
